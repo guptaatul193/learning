eee7bd7a40504f9bf778718064f365d6de67c16b
Atul Gupta <gupta.atul193@gmail.com>
2020-07-12 18:35:10 +0530

    added docstring to explaion first class functions

--- Python_Practice/Functions/test1.py
+++ Python_Practice/Functions/test1.py
@@ -1,4 +1,10 @@
-""" First-class functions """
+""" First-class functions 
+A function is an instance of the Object type.
+You can store the function in a variable.
+You can pass the function as a parameter to another function.
+You can return the function from a function.
+You can store them in data structures such as hash tables, lists,
+"""
 
 def func1():
 	msg = 'Hello'
@@ -17,8 +23,8 @@ def func2():
 	return inner_func
 
 func1()
-var_func2 = func2()
-var_func3 = func2()
+var_func2 = func2() # func2() is a first class function, and is stored liek an object in the variable var_func2
+var_func3 = func2() # func2() is a first class function, and is stored liek an object in the variable var_func3
 
 var_func2()
 var_func3()
