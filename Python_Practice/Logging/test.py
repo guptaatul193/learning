c4a7d76499c5292749052d9ca3fecf295e96ac4b
Atul Gupta <gupta.atul193@gmail.com>
2020-07-12 17:33:34 +0530

    added docstring

--- Python_Practice/Logging/test.py
+++ Python_Practice/Logging/test.py
@@ -1,9 +1,11 @@
 import logging, sys
 
+# setting file handler configurations
 log_fl_handler = logging.FileHandler(filename='argument_log.txt')
 log_fl_handler.setLevel(logging.DEBUG)
 log_fl_handler.setFormatter(logging.Formatter('%(asctime)s\t%(message)s'))
 
+# creating and setting logger configurations
 log_logger = logging.getLogger('argument_log')
 log_logger.setLevel(logging.DEBUG)
 log_logger.addHandler(log_fl_handler)
